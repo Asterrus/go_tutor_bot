89a3519463a4dcaea5f34c65d6a62c03cc8264f2
roman <gastursolar@gmail.com>
2025-10-17 03:03:11 +0500

    add tg bot

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 .PHONY: run
 
 run:
-	go run cmd/bot/main.go
+	
+	go run cmd/bot/main.go 
--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello")
+	godotenv.Load(".env")
+
+	token := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		if update.Message.IsCommand() {
+			handleCommand(bot, update.Message)
+		} else {
+			messageHandler(bot, update.Message)
+		}
+	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "hel p")
+	bot.Send(msg)
+}
+
+func unknownCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "unknown command")
+	bot.Send(msg)
+}
+func handleCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	switch inputMessage.Command() {
+	case "help":
+		helpCommand(bot, inputMessage)
+	default:
+		unknownCommand(bot, inputMessage)
+	}
+}
+
+func messageHandler(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote message: "+inputMessage.Text)
+	bot.Send(msg)
 }
