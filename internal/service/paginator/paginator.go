c3a44d22564d81e62c36ab16d1fa6037dbae7426
roman <gastursolar@gmail.com>
2025-10-18 23:24:49 +0500

    Paginator refactoring

--- internal/service/paginator/paginator.go
+++ internal/service/paginator/paginator.go
@@ -1,7 +1,6 @@
 package paginator
 
 import (
-	"fmt"
 	"math"
 )
 
@@ -15,21 +14,11 @@ func NewPaginator[T any](itemsOnPage int) *Paginator[T] {
 
 func (p *Paginator[T]) GetPaginatedItems(items []T, page int) []T {
 	startIndex := (page - 1) * p.ItemsOnPage
-	var endIndex int
-	if len(items) >= p.ItemsOnPage*page {
-		endIndex = p.ItemsOnPage * page
-	} else {
-		endIndex = len(items)
-	}
+	endIndex := min(p.ItemsOnPage*page, len(items))
 
 	return items[startIndex:endIndex]
 }
 
 func (p *Paginator[T]) TotalPages(items []T) int {
-	fmt.Println("p.ItemsOnPage", p.ItemsOnPage)
-	fmt.Println("len(items)", len(items))
-	fmt.Println("len(items) / p.ItemsOnPage", len(items)/p.ItemsOnPage)
-	fmt.Println("float64((len(items) / p.ItemsOnPage)", float64((len(items) / p.ItemsOnPage)))
-	fmt.Println("math.Ceil(float64((len(items) / p.ItemsOnPage)))", math.Ceil(float64((len(items) / p.ItemsOnPage))))
 	return int(math.Ceil((float64(len(items)) / float64(p.ItemsOnPage))))
 }
